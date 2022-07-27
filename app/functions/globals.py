c142a656183914b6a05dd3236efc37225eac1d94
nicholasosner <nic@innoventix.co.za>
2022-07-27 09:27:31 +0200

    Edit

--- app/functions/globals.py
+++ app/functions/globals.py
@@ -336,7 +336,7 @@ def importMonitor():
 
                         # Command to be sent to the image
                         userData  = 'cd /home/ubuntu/TrapTagger;'
-                        userData += ' git config --global --add safe.directory /home/ubuntu/TrapTagger;'
+                        userData += ' git config --global --add safe.directory .;'
                         userData += ' git fetch --all;'
                         userData += ' git checkout {};'.format(Config.QUEUES[queue]['branch'])
                         userData += ' git pull;'
