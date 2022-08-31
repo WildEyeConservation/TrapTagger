796eddec98368ff4f6e340a7d636a27ecf7ff0e6
nicholasosner <nic@innoventix.co.za>
2022-08-31 10:23:04 +0200

    Edit

--- app/routes.py
+++ app/routes.py
@@ -3150,7 +3150,7 @@ def login_page():
                 next_page = url_for('jobs', _external=True)
         return redirect(next_page)
 
-    return render_template('html/login.html', title='Sign In', form=form, helpFile='login', bucket=Config.Bucket)
+    return render_template('html/login.html', title='Sign In', form=form, helpFile='login', bucket=Config.BUCKET)
 
 @app.route('/load_login/<user_id>', methods=['GET', 'POST'])
 def load_login(user_id):
