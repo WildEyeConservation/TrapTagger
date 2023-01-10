c2993a9bb339daf98016ffae82fb3e4b7a1d2b4f
nicholasosner <nic@innoventix.co.za>
2023-01-10 10:36:44 +0200

    Edit

--- static/js/camtrap.fileUploader.js
+++ static/js/camtrap.fileUploader.js
@@ -276,13 +276,17 @@ function downloadFile(fileName,fileType) {
     }).then((response) => {
         return response.text()
     }).then((url) => {
-        return {
-            method: 'GET',
-            url: url,
-            fields: {},
-            headers: {
-                'Content-Type': fileType,
-            }
-        }
+        fetch(url)
+    }).then(response => 
+        response.blob()
+    ).then(blob => {
+        var url = window.URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
     })
 }
