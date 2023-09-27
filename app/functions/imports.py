0a2709e49e156c20e5439b877b0ddd1a8857f9a8
Hannes Naude <nicholasosner@gmail.com>
2023-09-27 08:53:53 +0000

    Added LILA data pipeline

--- app/functions/imports.py
+++ app/functions/imports.py
@@ -3739,3 +3739,115 @@ def batchCropping(self,images,source,min_area,destBucket,external,update_image_i
         db.session.remove()
 
     return True
+
+def convertBBox(api_box):
+    '''Function for converting MD format bboxes to our format'''
+    x_min, y_min, width_of_box, height_of_box = api_box
+    x_max = x_min + width_of_box
+    y_max = y_min + height_of_box
+    return [y_min, x_min, y_max, x_max]
+
+def commitAndCrop(images):
+    '''Helper function for pipelineLILA that commits the db and then kicks off image cropping'''
+    db.session.commit()
+    batchCropping.apply_async(kwargs={'images': [r.id for r in images],'source':source,'min_area':min_area,'destBucket':destBucket,'external':external,'update_image_info':update_image_info},queue='default')
+    return []
+
+@celery.task(bind=True,max_retries=29,ignore_result=True)
+def pipelineLILA(dets_filename,images_filename,survey_name,tgcode_str,source,min_area,destBucket):
+    '''Makes use of the MegaDetector results on LILA to pipeline trianing data.'''
+    try:
+        skip_labels = ['unknown','none','fire','human','null']
+        external = True
+        update_image_info=True
+        tgcode = re.compile(tgcode_str)
+
+        with tempfile.NamedTemporaryFile(delete=True, suffix='.json') as temp_file:
+            GLOBALS.s3client.download_file(Bucket=destBucket, Key=dets_filename, Filename=temp_file.name)
+            with open(temp_file.name) as f:
+                json_data = json.load(f)
+
+        with tempfile.NamedTemporaryFile(delete=True, suffix='.csv') as temp_file:
+            GLOBALS.s3client.download_file(Bucket=destBucket, Key=images_filename, Filename=temp_file.name)
+            df = pd.read_csv(temp_file.name)
+
+        if 'filename' in df.columns:
+            df = df.rename(columns={'filename': 'filepath','common_name': 'species'})
+
+        survey = Survey(name=survey_name,user_id=1,trapgroup_code=tgcode_str)
+        db.session.add(survey)
+        task = Task(name='import', survey=survey, tagging_level='-1', test_size=0, status='Ready')
+        db.session.add(task)
+        db.session.commit()
+        task_id=task.id
+        survey_id=survey.id
+
+        label_translations = {}
+        for description in df['species'].unique():
+            if description.lower() not in skip_labels:
+                if description.lower() in ['nothing','empty','blank']:
+                    label = db.session.query(Label).get(GLOBALS.nothing_id)
+                elif description.lower() in ['human']:
+                    label = db.session.query(Label).get(GLOBALS.vhl_id)
+                else:
+                    label = Label(description=description,task=task)
+                    db.session.add(label)
+                label_translations[description] = label
+        db.session.commit()
+
+        count = 0
+        images = []
+        trapgroups = {}
+        dirpath_cam_translations = {}
+        for item in json_data['images']:
+            if len(df[df['filepath']==item['file']]) < 2:
+                tags = tgcode.findall(item['file'])
+                if tags:
+                    tag = tags[0]
+                    species = [r for r in df[df['filepath']==item['file']]['species'].unique() if r.lower() not in skip_labels]
+                    dirpath = '/'.join(item['file'].split('/')[:-1])
+                    filename = item['file'].split('/')[-1]
+                    if tag not in trapgroups.keys():
+                        trapgroup = Trapgroup(survey_id=survey_id,tag=tag)
+                        db.session.add(trapgroup)
+                        images = commitAndCrop(images)
+                        trapgroups[tag] = trapgroup.id
+                    trapgroup_id = trapgroups[tag]
+                    if dirpath not in dirpath_cam_translations.keys():
+                        camera = Camera(path=dirpath,trapgroup_id=trapgroup_id)
+                        db.session.add(camera)
+                        images = commitAndCrop(images)
+                        dirpath_cam_translations[dirpath] = camera.id
+                    camera_id = dirpath_cam_translations[dirpath]
+                    image = Image(camera_id=camera_id,filename=filename)
+                    cluster = Cluster(task_id=task_id)
+                    db.session.add(image)
+                    db.session.add(cluster)
+                    cluster.images = [image]
+                    for specie in species:
+                        cluster.labels.append(label_translations[specie])
+                    images.append(image)
+                    for det in item['detections']:
+                        top, left, bottom, right = convertBBox(det['bbox'])
+                        detection = Detection(image=image,top=top,left=left,bottom=bottom,right=right,score=det['conf'],category=det['category'],source='MDv5b')
+                        db.session.add(detection)
+                        labelgroup = Labelgroup(task_id=task_id,detection=detection)
+                        db.session.add(labelgroup)
+                        for specie in species:
+                            labelgroup.labels.append(label_translations[specie])
+                    count += 1
+                    if count%100==0: print(count)
+        images = commitAndCrop(images)
+
+    except Exception as exc:
+        app.logger.info(' ')
+        app.logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        app.logger.info(traceback.format_exc())
+        app.logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        app.logger.info(' ')
+        self.retry(exc=exc, countdown= retryTime(self.request.retries))
+
+    finally:
+        db.session.remove()
+
+    return True
