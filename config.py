105822e03587692b5f1848871f52d1475e0b0ba1
MargueriteRobinson <grietsr@gmail.com>
2024-02-02 09:00:52 +0000

    Config edit

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     LOAD_TESTING = False
-    DEBUGGING = True
+    DEBUGGING = False
     MAINTENANCE = False
     INITIAL_SETUP = False
     VERSION = 25
