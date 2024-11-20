e06952898fb1709af2be060a757b53bba3f492c8
nicholasosner <nicholasosner@gmail.com>
2024-11-20 15:36:45 +0000

    Edit

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
     INITIAL_SETUP = False
     DISABLE_RESTORE = False
     KILL_FILEHANDLER = False
-    VERSION = 36
+    VERSION = 37
 
     HOST_IP = os.environ.get('HOST_IP')
     REDIS_IP = os.environ.get('REDIS_IP')
