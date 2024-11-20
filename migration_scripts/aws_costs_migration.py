568d114e269ed3459b46844f723611dda3f4b068
nicholasosner <nicholasosner@gmail.com>
2024-11-20 12:23:09 +0000

    Added archive column

--- migration_scripts/aws_costs_migration.py
+++ migration_scripts/aws_costs_migration.py
@@ -8,7 +8,7 @@ cutoff_date = datetime.now() - timedelta(days=30*13)
 cutoff_date = cutoff_date.replace(day=1,hour=0,minute=0,second=0,microsecond=0)
 for statistic in statistics:
     startDate = (statistic.timestamp - timedelta(days=10)).replace(day=1,hour=0,minute=0,second=0,microsecond=0)
-    endDate = statistic.timestamp.replace(day=1,hour=0,minute=0,second=0,microsecond=0)
+    endDate = statistic.timestamp.replace(day=1,hour=0,minute=0,second=0)
     if startDate < cutoff_date:
         statistic.textract_cost = 0
         statistic.other_cost = 0
