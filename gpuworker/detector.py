75d27d58082271a0cd0d87ebc2ad25994444a0ed
nicholasosner <nicholasosner@gmail.com>
2025-08-21 12:53:36 +0000

    Switched to MD package

--- gpuworker/detector.py
+++ gpuworker/detector.py
@@ -14,24 +14,29 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-import numpy as np
-from PIL import Image
 import tempfile
 import boto3
 import requests
 from botocore.exceptions import ClientError
+from megadetector.visualization import visualization_utils as vis_utils
+from megadetector.detection import run_detector
 
-init=False
 detector=None
-ct_utils=None
 s3client = boto3.client('s3')
 
 detectors = {
-    'MDv4': {'filename':"gpuworker/md_v4.1.0.pb"},
-    'MDv5a': {'filename':"md_v5a.0.0.pt"},
-    'MDv5b': {'filename':"md_v5b.0.0.pt"},
+    'MDv4': {'filename':"MDV4"}, #this will downloaded if needed
+    'MDv5a': {'filename':"md_v5a.0.1.pt"},
+    'MDv5b': {'filename':"md_v5b.0.1.pt"},
     'MDv1000redwood': {'filename':"md_v1000.0.0-redwood.pt"}
 }
+
+def convert_xywh_to_tlbr(bbox):
+    """Converts an xywh bounding box to an top, left, bottom, right box."""
+    x_min, y_min, width_of_box, height_of_box = bbox
+    x_max = x_min + width_of_box
+    y_max = y_min + height_of_box
+    return [y_min, x_min, y_max, x_max]
 
 def infer(batch,sourceBucket,external,model,threshold=0.05,pass_images=False):
     '''
@@ -49,23 +54,16 @@ def infer(batch,sourceBucket,external,model,threshold=0.05,pass_images=False):
             results (list): A list of detections for each image in the batch
     '''
 
-    # try:
-    print('Recieved batch of {} images.'.format(len(batch)))
-    global init,detector,ct_utils
-    if not(init):
-        print('Initialising')
-        import ct_utils
-        if detectors[model]['filename'].endswith('.pb'):
-            from detection.run_tf_detector import TFDetector
-            detector = TFDetector(detectors[model]['filename'])
-        elif detectors[model]['filename'].endswith('.pt'):
-            from detection.pytorch_detector import PTDetector
-            detector = PTDetector(detectors[model]['filename'])
-        init=True
+    try:
+        print('Recieved batch of {} images.'.format(len(batch)))
+        global detector
+        
+        if detector==None:
+            print('Initialising')
+            detector = run_detector.load_detector(detectors[model]['filename'])
 
-    if pass_images: images = {}
+        if pass_images: images = {}
 
-    if '4' not in model:
         results = []
         for image in batch:
             try:
@@ -119,7 +117,7 @@ def infer(batch,sourceBucket,external,model,threshold=0.05,pass_images=False):
                             continue
 
                     print('Done')
-                    img = Image.open(temp_file.name)
+                    img = vis_utils.load_image(temp_file.name)
                     if pass_images: images[url] = img
 
                 print('Generating detections...')
@@ -127,7 +125,7 @@ def infer(batch,sourceBucket,external,model,threshold=0.05,pass_images=False):
 
                 detections=[]
                 for detection in result['detections']:
-                    bbox = ct_utils.convert_xywh_to_tf(detection['bbox'])
+                    bbox = convert_xywh_to_tlbr(detection['bbox'])
                     detections.append(
                                 {'top':max(0.0, min(1.0, float(bbox[0]))),
                                 'left':max(0.0, min(1.0, float(bbox[1]))),
@@ -163,97 +161,21 @@ def infer(batch,sourceBucket,external,model,threshold=0.05,pass_images=False):
             
             results.append(detections)
 
-    else:
-        ######Local Download
-        imstack = []
-        for image in batch:
-            try:
-                with tempfile.NamedTemporaryFile(delete=True, suffix='.JPG') as temp_file:
-                    if external:
-                        print('Downloading {} from external source.'.format(image))
-                        attempts = 0
-                        retry = True
-                        while retry and (attempts < 10):
-                            attempts += 1
-                            try:
-                                if sourceBucket!='':
-                                    url = sourceBucket+'/'+image
-                                else:
-                                    url = image
-                                response = requests.get(url, timeout=30)
-                                assert (response.status_code==200) and ('image' in response.headers['content-type'].lower())
-                                retry = False
-                            except:
-                                retry = True
-                        with open(temp_file.name, 'wb') as handler:
-                            handler.write(response.content)
+        print('Finished batch')
 
-                    else:
-                        print('Downloading {} from S3'.format(image))
-                        url = image
-                        s3client.download_file(Bucket=sourceBucket, Key=image, Filename=temp_file.name)
-
-                    print('Done')
-                    img = Image.open(temp_file.name)
-                    if pass_images: images[url] = img
-                    imstack.append(np.asarray(img.resize((1024, 600)),np.uint8))
-                
-                print('Added to batch')
-            
-            except:
-                print('Failed to retrieve image {}'.format(image))
-            
-        imstack = np.stack(imstack)
-        
-        ######Blob Approach
-        # imstack = np.stack([np.asarray(Image.open(BytesIO(base64.b64decode(b64blob))).resize((1024, 600)),np.uint8) for b64blob in batch])
-        
-        print('Processing...')
-        (box_np, score_np, clss_np) = detector.tf_session.run([detector.box_tensor, detector.score_tensor, detector.class_tensor],feed_dict={detector.image_tensor: imstack})
-        print('Done')
-
+    except:
+        print('Error with batch... returning empty set of detections.')
         results=[]
-        print('Retrieving results...')
-        for i,fullname in enumerate(batch):
-            print('Next image')
-            detections=[]
-            for j, scr in enumerate(score_np[i, :]):
-                if scr < threshold:
-                    break
-                detections.append( {'top':max(0.0, min(1.0, float(box_np[i, j, 0]))),
-                                    'left':max(0.0, min(1.0, float(box_np[i, j, 1]))),
-                                    'bottom':max(0.0, min(1.0, float(box_np[i, j, 2]))),
-                                    'right':max(0.0, min(1.0, float(box_np[i, j, 3]))),
-                                    'category':int(clss_np[i, j]),
-                                    'score': float(score_np[i, j]),
-                                    'status': 'active',
-                                    'source' : model})
-            if len(detections)==0:
-                print('No detections found...')
-                detections.append( {'top':0.0,
+        for n in range(len(batch)):
+            detections=[{'top':0.0,
                                     'left':0.0,
                                     'bottom':0.0,
                                     'right':0.0,
                                     'category': 0,
                                     'score': 0.0,
                                     'status': 'active',
-                                    'source' : model})
+                                    'source' : 'error'}]
             results.append(detections)
-    print('Finished batch')
-
-    # except:
-    #     print('Error with batch... returning empty set of detections.')
-    #     results=[]
-    #     for n in range(len(batch)):
-    #         detections=[{'top':0.0,
-    #                                 'left':0.0,
-    #                                 'bottom':0.0,
-    #                                 'right':0.0,
-    #                                 'category': 0,
-    #                                 'score': 0.0,
-    #                                 'status': 'active',
-    #                                 'source' : 'error'}]
-    #         results.append(detections)
     
     if pass_images:
         return results, images
